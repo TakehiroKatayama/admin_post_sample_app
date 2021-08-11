206f3b221d03114211954b837d35900f3d7c31c0
TakehiroKatayama <t.katayama.info@gmail.com>
2021-08-11 23:14:02 +0900

    Newsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_130145) do
+ActiveRecord::Schema.define(version: 2021_08_11_141245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2021_08_11_130145) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
+  end
+
+  create_table "news", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.string "image", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
