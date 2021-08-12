965240dbf71d055003a5a1850ff8a274e27f9a89
TakehiroKatayama <t.katayama.info@gmail.com>
2021-08-12 22:49:51 +0900

    画像投稿用のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_141245) do
+ActiveRecord::Schema.define(version: 2021_08_12_134612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_08_11_141245) do
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
 end
